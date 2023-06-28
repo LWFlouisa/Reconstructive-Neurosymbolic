9430da4e34650612ee82b149bc8e1089e988f7c9
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-27 23:49:20 -0400

    Update reconstructor.rb

--- node2/Reconstructor/reconstructor.rb
+++ node2/Reconstructor/reconstructor.rb
@@ -1,22 +1,6 @@
 module Reconstructor
 
   class NeuralNet
-    def self.find_nuetral
-      # Make sure that story elements are kept on seperate lines.
-      character_fate = File.readlines("_narratives/outcomes/character_fates.txt")
-      dating_outcome = File.readlines("_narratives/outcomes/dating_outcomes.txt")
-
-      # Make the total imagined branch the size of the darkest path.
-      # branch_size = branch_1.size.to_i
-
-      # Imagined a compromise path that is neither ideal or tragic.
-      open("_imaginedpath/outcomes/nuetral_outcome.txt", "w") { |f|
-        segment_1 = character_fate[1].strip
-        segment_2 = dating_outcome[0].strip
-
-        f.puts "#{segment_1} #{segment_2}"
-      }
-    end
 
     def self.consult_dataset
       # If two nuetral outcomes agree, then retain.
